2ab0bae5cce25f0cbaad2c1993267a017a055890
wwhai <cnwwhai@gmail.com>
2024-05-08 20:55:07 +0800

    feat: add tencent iothub support

--- engine/engine.go
+++ engine/engine.go
@@ -488,6 +488,12 @@ func (e *RuleEngine) RestartDevice(uuid string) error {
  */
 
 func (e *RuleEngine) InitDeviceTypeManager() error {
+	e.DeviceTypeManager.Register(typex.TENCENT_IOTHUB_GATEWAY,
+		&typex.XConfig{
+			Engine:    e,
+			NewDevice: device.NewTencentIoTGateway,
+		},
+	)
 	e.DeviceTypeManager.Register(typex.SMART_HOME_CONTROLLER,
 		&typex.XConfig{
 			Engine:    e,
