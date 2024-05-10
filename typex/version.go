dd203ad5cc48c53f3947c9a274b80ccbacc55ab0
wwhai <cnwwhai@gmail.com>
2024-05-10 15:36:45 +0800

    feat: new api clearSchemaData

--- typex/version.go
+++ typex/version.go
@@ -24,10 +24,10 @@ var License = LocalLicense{
 
 var DefaultVersionInfo = VersionInfo{
 	Product:        "COMMON",
-	ReleaseTime: "2024-05-04 19:05:26",
+	ReleaseTime: "2024-05-10 15:32:37",
 }
 var Banner = `
  **  Welcome to RHILEX framework world <'_'>     -.
-**   Version: v0.7.0-bf36511cc5d35f9             -.
+**   Version: v0.7.0-4a55a45191de78e             -.
  **  More information: https://hootrhino.github.io  -.
 `
