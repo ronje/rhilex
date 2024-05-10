5e99916f201b6b667cdf0e239ccb2bb8a7b808e4
wwhai <cnwwhai@gmail.com>
2024-05-10 21:16:35 +0800

    dev: rename backup file

--- typex/version.go
+++ typex/version.go
@@ -24,10 +24,10 @@ var License = LocalLicense{
 
 var DefaultVersionInfo = VersionInfo{
 	Product:        "COMMON",
-	ReleaseTime: "2024-05-10 15:32:37",
+	ReleaseTime: "2024-05-10 20:39:25",
 }
 var Banner = `
  **  Welcome to RHILEX framework world <'_'>     -.
-**   Version: v0.7.0-4a55a45191de78e             -.
+**   Version: v0.7.0-4fcce87828007fb             -.
  **  More information: https://hootrhino.github.io  -.
 `
