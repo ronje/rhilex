8171d6e827ba2ffc40f6403c48a4472c2204e773
wwhai <cnwwhai@gmail.com>
2024-06-12 18:22:11 +0800

    dev: remove unused tests

--- test/knx_device_group_tunnel_test.go
+++ test/knx_device_group_tunnel_test.go
@@ -15,9 +15,57 @@
 
 package test
 
-import "testing"
+import (
+	"log"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/vapourismo/knx-go/knx"
+	"github.com/vapourismo/knx-go/knx/cemi"
+	"github.com/vapourismo/knx-go/knx/dpt"
+	"github.com/vapourismo/knx-go/knx/util"
+)
 
 // go test -timeout 30s -run ^Test_knx_group_tunnel github.com/hootrhino/rhilex/test -v -count=1
 func Test_knx_group_tunnel(t *testing.T) {
+	// Setup logger for auxiliary logging. This enables us to see log messages from internal
+	// routines.
+	util.Logger = log.New(os.Stdout, "", log.LstdFlags)
 
+	// Connect to the gateway.
+	client, err := knx.NewGroupTunnel("127.0.0.1:3671", knx.TunnelConfig{
+		ResendInterval:    500 * time.Millisecond,
+		HeartbeatInterval: 10 * time.Second,
+		ResponseTimeout:   10 * time.Second,
+		SendLocalAddress:  true,
+		UseTCP:            false,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Close upon exiting. Even if the gateway closes the connection, we still have to clean up.
+	defer client.Close()
+
+	// Send 20.5°C to group 1/2/3.
+	err = client.Send(knx.GroupEvent{
+		Command:     knx.GroupWrite,
+		Destination: cemi.NewGroupAddr3(1, 2, 3),
+		Data:        dpt.DPT_9001(20.5).Pack(),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Receive messages from the gateway. The inbound channel is closed with the connection.
+	for msg := range client.Inbound() {
+		var temp dpt.DPT_9001
+
+		err := temp.Unpack(msg.Data)
+		if err != nil {
+			continue
+		}
+
+		util.Logger.Printf("%+v: %v", msg, temp)
+	}
 }
