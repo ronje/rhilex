8a1ce59af81b650292f035fba5b53a5708a8e5e0
wwhai <cnwwhai@gmail.com>
2024-07-28 14:31:27 +0800

    dev: update return result

--- component/apiserver/apis/device_modbus_slaver_sheet_api.go
+++ component/apiserver/apis/device_modbus_slaver_sheet_api.go
@@ -36,9 +36,10 @@ type ModbusSlaverRegister struct {
 	Value   interface{} `json:"value"`
 }
 type ModbusSlaverRegisterVo struct {
-	Coils            []ModbusSlaverRegister `json:"coils"`
-	HoldingRegisters []ModbusSlaverRegister `json:"holdingRegisters"`
-	InputRegisters   []ModbusSlaverRegister `json:"inputRegisters"`
+	Coils             []ModbusSlaverRegister `json:"coils"`
+	DiscreteRegisters []ModbusSlaverRegister `json:"discreteRegisters"`
+	HoldingRegisters  []ModbusSlaverRegister `json:"holdingRegisters"`
+	InputRegisters    []ModbusSlaverRegister `json:"inputRegisters"`
 }
 
 func ModbusSlaverSheetPageList(c *gin.Context, ruleEngine typex.Rhilex) {
@@ -53,6 +54,8 @@ func ModbusSlaverSheetPageList(c *gin.Context, ruleEngine typex.Rhilex) {
 		c.JSON(common.HTTP_OK, common.Error("Cache Slot Not Exists"))
 		return
 	}
+	Type, _ := c.GetQuery("registerType")
+
 	// 1: 线圈寄存器      Coils Registers
 	// 2: 离散寄存器      Discrete Registers
 	// 3: 保持寄存器      Holding Registers
@@ -60,52 +63,82 @@ func ModbusSlaverSheetPageList(c *gin.Context, ruleEngine typex.Rhilex) {
 	Coils := []ModbusSlaverRegister{}
 	HoldingRegisters := []ModbusSlaverRegister{}
 	InputRegisters := []ModbusSlaverRegister{}
-	for i := 0; i < 64; i++ {
-		UUID := fmt.Sprintf("%s_Coils:%d", deviceUuid, i)
-		Register := ModbusSlaverRegister{
-			UUID:    UUID,
-			Type:    1,
-			Address: i,
-			Value:   0,
+	DiscreteRegisters := []ModbusSlaverRegister{}
+	if Type == "Coils" {
+		for i := 0; i < 64; i++ {
+			UUID := fmt.Sprintf("%s_Coils:%d", deviceUuid, i)
+			Register := ModbusSlaverRegister{
+				UUID:    UUID,
+				Type:    1,
+				Address: i,
+				Value:   0,
+			}
+			Value, ok := Slot[UUID]
+			if ok {
+				Register.Value = Value.Value
+			}
+			Coils = append(Coils, Register)
 		}
-		Value, ok := Slot[UUID]
-		if ok {
-			Register.Value = Value.Value
-		}
-		Coils = append(Coils, Register)
+		Result := service.WrapPageResult(*pager, Coils, 64)
+		c.JSON(common.HTTP_OK, common.OkWithData(Result))
+		return
 	}
-	for i := 0; i < 64; i++ {
-		UUID := fmt.Sprintf("%s_HoldingRegisters:%d", deviceUuid, i)
-		Register := ModbusSlaverRegister{
-			UUID:    UUID,
-			Type:    1,
-			Address: i,
-			Value:   0,
+	if Type == "Discrete" {
+		for i := 0; i < 64; i++ {
+			UUID := fmt.Sprintf("%s_DiscreteRegisters:%d", deviceUuid, i)
+			Register := ModbusSlaverRegister{
+				UUID:    UUID,
+				Type:    1,
+				Address: i,
+				Value:   0,
+			}
+			Value, ok := Slot[UUID]
+			if ok {
+				Register.Value = Value.Value
+			}
+			DiscreteRegisters = append(DiscreteRegisters, Register)
 		}
-		Value, ok := Slot[UUID]
-		if ok {
-			Register.Value = Value.Value
-		}
-		HoldingRegisters = append(Coils, Register)
+		Result := service.WrapPageResult(*pager, DiscreteRegisters, 64)
+		c.JSON(common.HTTP_OK, common.OkWithData(Result))
+		return
 	}
-	for i := 0; i < 64; i++ {
-		UUID := fmt.Sprintf("%s_InputRegisters:%d", deviceUuid, i)
-		Register := ModbusSlaverRegister{
-			UUID:    UUID,
-			Type:    1,
-			Address: i,
-			Value:   0,
+	if Type == "Holding" {
+		for i := 0; i < 64; i++ {
+			UUID := fmt.Sprintf("%s_HoldingRegisters:%d", deviceUuid, i)
+			Register := ModbusSlaverRegister{
+				UUID:    UUID,
+				Type:    1,
+				Address: i,
+				Value:   0,
+			}
+			Value, ok := Slot[UUID]
+			if ok {
+				Register.Value = Value.Value
+			}
+			HoldingRegisters = append(HoldingRegisters, Register)
 		}
-		Value, ok := Slot[UUID]
-		if ok {
-			Register.Value = Value.Value
-		}
-		InputRegisters = append(Coils, Register)
+		Result := service.WrapPageResult(*pager, HoldingRegisters, 64)
+		c.JSON(common.HTTP_OK, common.OkWithData(Result))
+		return
 	}
-	Result := service.WrapPageResult(*pager, ModbusSlaverRegisterVo{
-		Coils:            Coils,
-		HoldingRegisters: HoldingRegisters,
-		InputRegisters:   InputRegisters,
-	}, 64)
-	c.JSON(common.HTTP_OK, common.OkWithData(Result))
+	if Type == "Input" {
+		for i := 0; i < 64; i++ {
+			UUID := fmt.Sprintf("%s_InputRegisters:%d", deviceUuid, i)
+			Register := ModbusSlaverRegister{
+				UUID:    UUID,
+				Type:    1,
+				Address: i,
+				Value:   0,
+			}
+			Value, ok := Slot[UUID]
+			if ok {
+				Register.Value = Value.Value
+			}
+			InputRegisters = append(InputRegisters, Register)
+		}
+		Result := service.WrapPageResult(*pager, InputRegisters, 64)
+		c.JSON(common.HTTP_OK, common.OkWithData(Result))
+		return
+	}
+	c.JSON(common.HTTP_OK, common.Error("unsupported register type"))
 }
