a09b4fcc0615ea17ec5be03866feefccdeb50b26
wwhai <cnwwhai@gmail.com>
2024-10-19 20:13:10 +0800

    dev: change crc polygon

--- device/szy2062016/misc.go
+++ device/szy2062016/misc.go
@@ -15,14 +15,25 @@
 
 package szy2062016
 
-/**
- * CRC
- *
- */
-func crc8(data []byte) byte {
-	var checksum byte
+const (
+	// CRC多项式X^7 + X^6 + X^5 + X^2 + 1的二进制表示为0b1110001
+	polynomial = 0xE1 // 0b11100001
+)
+
+// 计算CRC校验码
+func crc(data []byte) byte {
+	// 初始值
+	crc := byte(0)
+
 	for _, b := range data {
-		checksum += b
+		crc ^= b                 // XOR当前字节
+		for i := 0; i < 8; i++ { // 处理8位
+			if (crc & 0x80) != 0 { // 如果最高位为1
+				crc = (crc << 1) ^ polynomial // 左移并XOR多项式
+			} else {
+				crc <<= 1 // 仅左移
+			}
+		}
 	}
-	return checksum
+	return crc
 }
