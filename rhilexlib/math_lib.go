1c0cc74696a6a999086345d52f15d90ecbcd4dc7
wwhai <cnwwhai@gmail.com>
2024-12-09 17:49:13 +0800

    dev: add math \ string lib

--- rhilexlib/math_lib.go
+++ rhilexlib/math_lib.go
@@ -21,49 +21,142 @@ import (
 	"time"
 
 	lua "github.com/hootrhino/gopher-lua"
-	"github.com/hootrhino/rhilex/typex"
 )
-
-/*``
-*
-* XOR 校验
-*
- */
-func RandomInt(rx typex.Rhilex, uuid string) func(*lua.LState) int {
-	return func(L *lua.LState) int {
-		v := L.ToNumber(2)
-		L.Push(lua.LNumber(randomInt(int(v))))
-		return 1
-	}
-}
 
 /*
 *
 *随机数0-N
 *
  */
-func randomInt(v int) int {
+func MathRandomInt(L *lua.LState) int {
+	v := L.ToNumber(2)
 	source := rand.NewSource(time.Now().UnixNano())
 	rng := rand.New(source)
-	return rng.Intn(v) + 1
+	r := rng.Intn(int(v)) + 1
+	L.Push(lua.LNumber(r))
+	return 1
 }
 
-func truncateFloat(number float64, decimalPlaces int) float64 {
-	scale := math.Pow(10, float64(decimalPlaces))
-	result := math.Floor(number*scale) / scale
-	return result
+func MathAbs(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Abs(float64(arg))))
+	return 1
 }
 
-/*
-*
-* 取小数位 applib:Float(number, decimalPlaces) -> float
-*
- */
-func TruncateFloat(rx typex.Rhilex, uuid string) func(*lua.LState) int {
-	return func(l *lua.LState) int {
-		number := l.ToNumber(2)
-		decimalPlaces := l.ToInt(3)
-		l.Push(lua.LNumber(truncateFloat(float64(number), decimalPlaces)))
+func MathAcos(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Acos(float64(arg))))
+	return 1
+}
+
+func MathAsin(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Asin(float64(arg))))
+	return 1
+}
+
+func MathAtan(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Atan(float64(arg))))
+	return 1
+}
+
+func MathAtan2(L *lua.LState) int {
+	y := L.CheckNumber(1)
+	x := L.CheckNumber(2)
+	L.Push(lua.LNumber(math.Atan2(float64(y), float64(x))))
+	return 1
+}
+
+func MathCeil(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Ceil(float64(arg))))
+	return 1
+}
+
+func MathCos(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Cos(float64(arg))))
+	return 1
+}
+
+func MathExp(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Exp(float64(arg))))
+	return 1
+}
+
+func MathFloor(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Floor(float64(arg))))
+	return 1
+}
+
+func MathLog(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Log(float64(arg))))
+	return 1
+}
+
+func MathMax(L *lua.LState) int {
+	if L.GetTop() == 0 {
+		L.Push(lua.LNumber(math.Inf(1)))
 		return 1
 	}
+	max := L.CheckNumber(1)
+	for i := 2; i <= L.GetTop(); i++ {
+		arg := L.CheckNumber(i)
+		if arg > max {
+			max = arg
+		}
+	}
+	L.Push(lua.LNumber(max))
+	return 1
+}
+
+func MathMin(L *lua.LState) int {
+	if L.GetTop() == 0 {
+		L.Push(lua.LNumber(math.Inf(-1)))
+		return 1
+	}
+	min := L.CheckNumber(1)
+	for i := 2; i <= L.GetTop(); i++ {
+		arg := L.CheckNumber(i)
+		if arg < min {
+			min = arg
+		}
+	}
+	L.Push(lua.LNumber(min))
+	return 1
+}
+
+func MathPow(L *lua.LState) int {
+	base := L.CheckNumber(1)
+	exp := L.CheckNumber(2)
+	L.Push(lua.LNumber(math.Pow(float64(base), float64(exp))))
+	return 1
+}
+
+func MathSin(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Sin(float64(arg))))
+	return 1
+}
+
+func MathSqrt(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Sqrt(float64(arg))))
+	return 1
+}
+
+func MathTan(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Tan(float64(arg))))
+	return 1
+}
+
+func MathRound(L *lua.LState) int {
+	arg := L.CheckNumber(1)
+	L.Push(lua.LNumber(math.Round(float64(arg))))
+	return 1
 }
--- rhilexlib/string_lib.go
+++ rhilexlib/string_lib.go
@@ -3,8 +3,6 @@ package rhilexlib
 import (
 	"strings"
 
-	"github.com/hootrhino/rhilex/typex"
-
 	lua "github.com/hootrhino/gopher-lua"
 )
 
@@ -13,35 +11,107 @@ import (
 * Table 转成 String, {1,2,3,4,5} -> "12345"
 *
  */
-func T2Str(rx typex.Rhilex, uuid string) func(*lua.LState) int {
-	return func(l *lua.LState) int {
-		table := l.ToTable(2)
-		args := []string{}
-		table.ForEach(func(l1, value lua.LValue) {
-			args = append(args, value.String())
-		})
-		r := strings.Join(args, "")
-		l.Push(lua.LString(r))
-		return 1
-	}
+func T2Str(l *lua.LState) int {
+	table := l.ToTable(1)
+	args := []string{}
+	table.ForEach(func(l1, value lua.LValue) {
+		args = append(args, value.String())
+	})
+	r := strings.Join(args, "")
+	l.Push(lua.LString(r))
+	return 1
 }
 
 // {255,255,255} -> "\0xFF\0xFF\0xFF"
-func Bin2Str(rx typex.Rhilex, uuid string) func(*lua.LState) int {
-	return func(l *lua.LState) int {
-		table := l.ToTable(2)
-		args := []byte{}
-		table.ForEach(func(l1, value lua.LValue) {
-			switch value.Type() {
-			case lua.LTNumber:
-				if lua.LVAsNumber(value) >= 0 && lua.LVAsNumber(value) <= 255 {
-					args = append(args, byte(lua.LVAsNumber(value)))
-				}
-			default:
-				return
+func Bin2Str(l *lua.LState) int {
+	table := l.ToTable(1)
+	args := []byte{}
+	table.ForEach(func(l1, value lua.LValue) {
+		switch value.Type() {
+		case lua.LTNumber:
+			if lua.LVAsNumber(value) >= 0 && lua.LVAsNumber(value) <= 255 {
+				args = append(args, byte(lua.LVAsNumber(value)))
 			}
-		})
-		l.Push(lua.LString(string(args)))
-		return 1
-	}
+		default:
+			return
+		}
+	})
+	l.Push(lua.LString(string(args)))
+	return 1
+}
+
+// StringToUpper 将字符串转换为大写
+func StringToUpper(L *lua.LState) int {
+	s := L.ToString(1)
+	L.Push(lua.LString(strings.ToUpper(s)))
+	return 1
+}
+
+// StringToLower 将字符串转换为小写
+func StringToLower(L *lua.LState) int {
+	s := L.ToString(1)
+	L.Push(lua.LString(strings.ToLower(s)))
+	return 1
+}
+
+// StringTrim 去除字符串两端的空白字符
+func StringTrim(L *lua.LState) int {
+	s := L.ToString(1)
+	L.Push(lua.LString(strings.TrimSpace(s)))
+	return 1
+}
+
+// StringTrimLeft 去除字符串左侧的空白字符
+func StringTrimLeft(L *lua.LState) int {
+	s := L.ToString(1)
+	L.Push(lua.LString(strings.TrimLeft(s, " ")))
+	return 1
+}
+
+// StringTrimRight 去除字符串右侧的空白字符
+func StringTrimRight(L *lua.LState) int {
+	s := L.ToString(1)
+	L.Push(lua.LString(strings.TrimRight(s, " ")))
+	return 1
+}
+
+// StringReplace 替换字符串中的子串
+func StringReplace(L *lua.LState) int {
+	s := L.ToString(1)
+	old := L.ToString(2)
+	new := L.ToString(3)
+	L.Push(lua.LString(strings.Replace(s, old, new, -1)))
+	return 1
+}
+
+// StringRepeat 重复字符串多次
+func StringRepeat(L *lua.LState) int {
+	s := L.ToString(1)
+	count := L.ToInt(2)
+	L.Push(lua.LString(strings.Repeat(s, count)))
+	return 1
+}
+
+// StringContains 检查字符串是否包含子串
+func StringContains(L *lua.LState) int {
+	s := L.ToString(1)
+	substr := L.ToString(2)
+	L.Push(lua.LBool(strings.Contains(s, substr)))
+	return 1
+}
+
+// StringHasPrefix 检查字符串是否以指定前缀开始
+func StringHasPrefix(L *lua.LState) int {
+	s := L.ToString(1)
+	prefix := L.ToString(2)
+	L.Push(lua.LBool(strings.HasPrefix(s, prefix)))
+	return 1
+}
+
+// StringHasSuffix 检查字符串是否以指定后缀结束
+func StringHasSuffix(L *lua.LState) int {
+	s := L.ToString(1)
+	suffix := L.ToString(2)
+	L.Push(lua.LBool(strings.HasSuffix(s, suffix)))
+	return 1
 }
